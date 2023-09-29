a49c1495bba29e692c08afa5ae8042c07648d0e5
RaihanMahmud-514 <raihan.mahmud514@gmail.com>
2023-09-29 12:58:13 +0600

    Iteration of products

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -17,7 +17,7 @@ describe('My First Test Suit', function()
             cy.get('.search-keyword').type('ca')
             cy.wait(2000)
             cy.get('.products').find('.product').should('have.length', 4)
-            cy.wait(2000)
+            cy.wait(2000)   
             cy.get('.products').find('.product').eq('2').contains('ADD TO CART').click()
 
         })
