f49e68455ab5ac3d49b97701c90365c542fbfafe
RaihanMahmud-514 <raihan.mahmud514@gmail.com>
2023-10-02 22:24:21 +0600

    Get the text of the logo

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -16,15 +16,21 @@ describe('My First Test Suit', function()
             cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
             cy.get('.search-keyword').type('ca')
             cy.wait(2000)
+            //Parent-Child Chaining
             cy.get('.products').find('.product').should('have.length', 4)
             cy.wait(2000)   
             cy.get('.products').find('.product').eq('2').contains('ADD TO CART').click()
-
+            console.log('Checkingk asyncronous')
             cy.get('.products').find('.product').each(($e1, index, $list) => {
                 const textveg=$e1.find('h4.product-name').text();
                 if(textveg.includes('Cashews')){
                     cy.wrap($e1).find('button').click();
                 }
+            })
+            //Get the text of the logo
+            cy.get('.brand').then(function(logoelement)
+            {
+                cy.log(logoelement.text())
             })
 
         })
