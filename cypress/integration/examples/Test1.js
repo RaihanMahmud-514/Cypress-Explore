26db41c1caff409248c11d916e78e553ede1b44c
RaihanMahmud-514 <raihan.mahmud514@gmail.com>
2023-10-03 23:21:20 +0600

    Aliasing and Assertion

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -16,18 +16,25 @@ describe('My First Test Suit', function()
             cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
             cy.get('.search-keyword').type('ca')
             cy.wait(2000)
-            //Parent-Child Chaining
-            cy.get('.products').find('.product').should('have.length', 4)
+            //Parent-Child Chaining & Aliasing
+            cy.get('.products').as('productLocator')
+            cy.get('@productLocator').find('.product').should('have.length', 4)
             cy.wait(2000)   
-            cy.get('.products').find('.product').eq('2').contains('ADD TO CART').click()
-            console.log('Checkingk asyncronous')
-            cy.get('.products').find('.product').each(($e1, index, $list) => {
+            cy.get(':nth-child(3) > .product-action > button').click()
+            cy.get('@productLocator').find('.product').eq('2').contains('ADD TO CART').click().then(function()
+            {
+                console.log('Checking asyncronous')
+            })
+            
+            cy.get('@productLocator').find('.product').each(($e1, index, $list) => {
                 const textveg=$e1.find('h4.product-name').text();
                 if(textveg.includes('Cashews')){
                     cy.wrap($e1).find('button').click();
                 }
             })
-            //Get the text of the logo
+            //Assert logo text
+            cy.get('.brand').should('have.text', 'GREENKART')
+            //Get the text of the logo and this is to print in the logs
             cy.get('.brand').then(function(logoelement)
             {
                 cy.log(logoelement.text())
